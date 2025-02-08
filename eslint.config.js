75a82e115ee6e650b557d6c6b16df7cd5380c8fa
Justin Tung <justin.tung@live.ca>
2025-02-07 23:25:29 -0500

    build(deps-dev): bump eslint, sharp

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,10 @@
 import pluginJs from "@eslint/js";
 import globals from "globals";
+import tseslint from "typescript-eslint";
 
 export default [
   pluginJs.configs.recommended,
-
+  ...tseslint.configs.recommended,
   {
     rules: {
       "no-unused-vars": "warn",
